572430d137053fc8d17383bf70997d5f38d4ac1a
Sky Kosiner <sky@skykosiner.co.uk>
2023-07-16 22:40:39 +0100

    feat: Seprated footer it it's own file

--- pages/js/navFooter.js
+++ pages/js/navFooter.js
@@ -6,3 +6,12 @@ fetch("/html/nav.html")
     newelem.innerHTML = text;
     oldelem.parentNode.replaceChild(newelem,oldelem);
 })
+
+fetch("/html/footer.html")
+.then(res => res.text())
+.then(text => {
+    let oldelem = document.querySelector("script#replace_with_footer");
+    let newelem = document.createElement("div");
+    newelem.innerHTML = text;
+    oldelem.parentNode.replaceChild(newelem,oldelem);
+})
