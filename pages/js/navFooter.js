248a1867d68a41475b80187195d7eb08167f6d0e
Sky Kosiner <sky@skykosiner.co.uk>
2023-07-17 00:43:14 +0100

    feat: hamburger menu

--- pages/js/navFooter.js
+++ pages/js/navFooter.js
@@ -1,17 +1,19 @@
-fetch("/html/nav.html")
-.then(res => res.text())
-.then(text => {
-    let oldelem = document.querySelector("script#replace_with_navbar");
-    let newelem = document.createElement("div");
-    newelem.innerHTML = text;
-    oldelem.parentNode.replaceChild(newelem,oldelem);
-})
+window.onload = function() {
+    fetch("/html/nav.html")
+        .then(res => res.text())
+        .then(text => {
+            const oldelem = document.querySelector("script#replace_with_navbar");
+            const newelem = document.createElement("div");
+            newelem.innerHTML = text;
+            oldelem.parentNode.replaceChild(newelem, oldelem);
+        })
 
-fetch("/html/footer.html")
-.then(res => res.text())
-.then(text => {
-    let oldelem = document.querySelector("script#replace_with_footer");
-    let newelem = document.createElement("div");
-    newelem.innerHTML = text;
-    oldelem.parentNode.replaceChild(newelem,oldelem);
-})
+    fetch("/html/footer.html")
+        .then(res => res.text())
+        .then(text => {
+            let oldelem = document.querySelector("script#replace_with_footer");
+            let newelem = document.createElement("div");
+            newelem.innerHTML = text;
+            oldelem.parentNode.replaceChild(newelem,oldelem);
+        })
+};
