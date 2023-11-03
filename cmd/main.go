82bcbdc8eae80383e995b369a7e6137f7d7e0004
Sky Kosiner <sky@skykosiner.co.uk>
2023-11-03 18:17:04 +0000

    feat: clean up files

--- cmd/main.go
+++ cmd/main.go
@@ -2,95 +2,16 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"path"
-	"strings"
 
 	"github.com/joho/godotenv"
-	"github.com/russross/blackfriday"
 	"github.com/skykosiner-com/pkg/blog"
 	"github.com/skykosiner-com/pkg/book"
 	"github.com/skykosiner-com/pkg/utils"
 )
-
-type BlogPost404 struct {
-	title string
-	blurb string
-}
-
-func Get404BlogPostsRecs(w http.ResponseWriter, r *http.Request) {
-	var postsArr []BlogPost404
-	posts, err := os.ReadDir("./blog/publish")
-
-	if err != nil {
-		log.Fatal("Error getting blurbs for post ", err)
-	}
-
-	for _, post := range posts {
-		var blurbArr []string
-		var blurb string
-		title := post.Name()
-		file, err := os.Open(fmt.Sprintf("./blog/publish/%s", title))
-
-		if err != nil {
-			log.Fatal("Error getting blurbs for post", err)
-		}
-
-		defer file.Close()
-
-		bytes, err := ioutil.ReadAll(file)
-
-		if err != nil {
-			log.Fatal("Error getting blurbs for post", err)
-		}
-
-		lines := strings.Split(string(bytes), "\n")
-
-		for idx, line := range lines {
-			if idx == 0 || idx == 1 {
-				continue
-			}
-
-			if len(blurbArr) > 4 {
-				break
-			}
-
-			blurbArr = append(blurbArr, line)
-			blurb += line
-		}
-
-		postsArr = append(postsArr, BlogPost404{strings.Split(title, ".md")[0], string(blackfriday.MarkdownBasic([]byte(blurb)))})
-	}
-
-	fmt.Fprintf(w, "%s", postsArr)
-}
-
-func Contact(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Redirect(w, r, "/html/contact.html", http.StatusSeeOther)
-	}
-
-	if strings.Contains(r.FormValue("name"), "Mike") {
-		http.Redirect(w, r, "/html/spam.html", http.StatusSeeOther)
-		return
-	}
-
-	msg := fmt.Sprintf(`To: "Sky Kosiner" <sky@skykosiner.com>
-From: "Sky Kosiner" <ykosiner@gmail.com>
-Subject: Contact Form skykosiner.com | %s`, r.FormValue("name"))
-	body := []byte(fmt.Sprintf("%s\nName %s\nEmail %s\nMessage\n%s", msg, r.FormValue("name"), r.FormValue("email"), r.FormValue("message")))
-
-	err := utils.SendMail(body)
-
-	if err != nil {
-		fmt.Fprintf(w, "Error sending email %s", err)
-	} else {
-		http.Redirect(w, r, "/contact.html", http.StatusOK)
-	}
-}
 
 // File server with a custom 404 page instead defualt golang page of 404 not found
 func FileServerWithCustom404(fs http.FileSystem) http.Handler {
@@ -126,9 +47,9 @@ func main() {
 	http.HandleFunc("/getPosts/", utils.ListBlogPosts)
 	// http.HandleFunc("/getPostsWithDate/", utils.ListBlogPostsWithDate)
 	http.HandleFunc("/getBooks/", utils.ListBookNotes)
-	http.HandleFunc("/contact/", Contact)
+	http.HandleFunc("/contact/", utils.Contact)
 	http.HandleFunc("/search", blog.SearchHandler)
-	http.HandleFunc("/Get404BlogPostsRecs/", Get404BlogPostsRecs)
+	http.HandleFunc("/Get404BlogPostsRecs/", utils.Get404BlogPostsRecs)
 
 	fmt.Println(fmt.Sprintf("Listening on port %s", port))
 	log.Fatal(http.ListenAndServe(port, nil))
--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/smtp"
@@ -13,9 +14,14 @@ import (
 	"github.com/russross/blackfriday"
 )
 
+type BlogPost404 struct {
+	title string
+	blurb string
+}
+
 func SendMail(body []byte) error {
 	auth := smtp.PlainAuth("", "ykosiner@gmail.com", os.Getenv("GMAIL_PASSWORD"), "smtp.gmail.com")
-	to := []string{"ykosiner@gmail.com"}
+	to := []string{"sky@skykosiner.com"}
 	err := smtp.SendMail("smtp.gmail.com:587", auth, "ykosiner@gmail.com", to, body)
 
 	return err
@@ -115,4 +121,75 @@ func ListBlogPosts(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Fprintf(w, "%s", strReturn)
+}
+
+func Get404BlogPostsRecs(w http.ResponseWriter, r *http.Request) {
+	var postsArr []BlogPost404
+	posts, err := os.ReadDir("./blog/publish")
+
+	if err != nil {
+		log.Fatal("Error getting blurbs for post ", err)
+	}
+
+	for _, post := range posts {
+		var blurbArr []string
+		var blurb string
+		title := post.Name()
+		file, err := os.Open(fmt.Sprintf("./blog/publish/%s", title))
+
+		if err != nil {
+			log.Fatal("Error getting blurbs for post", err)
+		}
+
+		defer file.Close()
+
+		bytes, err := ioutil.ReadAll(file)
+
+		if err != nil {
+			log.Fatal("Error getting blurbs for post", err)
+		}
+
+		lines := strings.Split(string(bytes), "\n")
+
+		for idx, line := range lines {
+			if idx == 0 || idx == 1 {
+				continue
+			}
+
+			if len(blurbArr) > 4 {
+				break
+			}
+
+			blurbArr = append(blurbArr, line)
+			blurb += line
+		}
+
+		postsArr = append(postsArr, BlogPost404{strings.Split(title, ".md")[0], string(blackfriday.MarkdownBasic([]byte(blurb)))})
+	}
+
+	fmt.Fprintf(w, "%s", postsArr)
+}
+
+func Contact(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Redirect(w, r, "/html/contact.html", http.StatusSeeOther)
+	}
+
+	if strings.Contains(r.FormValue("name"), "Mike") {
+		http.Redirect(w, r, "/html/spam.html", http.StatusSeeOther)
+		return
+	}
+
+	msg := fmt.Sprintf(`To: "Sky Kosiner" <sky@skykosiner.com>
+From: "Sky Kosiner" <ykosiner@gmail.com>
+Subject: Contact Form skykosiner.com | %s`, r.FormValue("name"))
+	body := []byte(fmt.Sprintf("%s\nName %s\nEmail %s\nMessage\n%s", msg, r.FormValue("name"), r.FormValue("email"), r.FormValue("message")))
+
+	err := SendMail(body)
+
+	if err != nil {
+		fmt.Fprintf(w, "Error sending email %s", err)
+	} else {
+		http.Redirect(w, r, "/html/contact.html", http.StatusOK)
+	}
 }
