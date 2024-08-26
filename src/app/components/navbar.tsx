82f3c96dbb31ed2d867b5023d0807526be12619a
Sky Kosiner <sky@skykosiner.com>
2024-08-26 18:51:56 +0100

    feat(nav): Done base desktop navbar

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,10 +1,12 @@
 import Link from "next/link";
 import styles from "./navbar.module.css";
+import Image from "next/image";
 
 export default function NavBar(): JSX.Element {
     return (
         <div className={styles.nav}>
             <div className={styles.image}>
+                <Image src="/logo.svg" width="80" height="80" alt="logo" />
             </div>
             <div className={styles.links}>
                 <Link href={`/about`}>About</Link>
@@ -13,5 +15,5 @@ export default function NavBar(): JSX.Element {
                 <Link href={`/blog`}>Blog</Link>
             </div>
         </div>
-    )
+    );
 }
