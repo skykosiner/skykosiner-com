6d69a916217f3d70a629445146210612e3b3f00b
Sky Kosiner <sky@skykosiner.com>
2024-08-26 19:15:05 +0100

    feat: Landing page hero done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
+import styles from "./home.module.css";
 
 export default function Home(): JSX.Element {
     return (
-        <div>
-            <div>
+        <div className="center">
+            <div className={styles.hero}>
                 <h1>Hey Guys, I'm Sky.</h1>
                 <h2>I'm a self taught <a href="https://github.com/skykosiner" target=" _blank">developer</a> and <Link href="/">content creator</Link>.</h2>
                 <p>On this website you can learn more <Link href="/about">about me</Link>, find out what I'm working on, and <Link href="/contact">contact me.</Link></p>
