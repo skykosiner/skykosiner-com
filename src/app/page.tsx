b74b7941429de7fe7c6bf020053f954cee5e043f
Sky Kosiner <sky@skykosiner.com>
2024-08-26 19:09:13 +0100

    feat: Base landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+import Link from "next/link";
+
 export default function Home(): JSX.Element {
     return (
         <div>
-            <h1>Hello World</h1>
+            <div>
+                <h1>Hey Guys, I'm Sky.</h1>
+                <h2>I'm a self taught <a href="https://github.com/skykosiner" target=" _blank">developer</a> and <Link href="/">content creator</Link>.</h2>
+                <p>On this website you can learn more <Link href="/about">about me</Link>, find out what I'm working on, and <Link href="/contact">contact me.</Link></p>
+            </div>
+
+            <div>
+            </div>
         </div>
     );
 }
