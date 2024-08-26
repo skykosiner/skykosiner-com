c758edef1fbd9e8dceadb664db66a0e8f04c18c5
Sky Kosiner <sky@skykosiner.com>
2024-08-26 19:18:51 +0100

    feat(navbar): Updated the links and hover stuff

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -6,11 +6,14 @@ export default function NavBar(): JSX.Element {
     return (
         <div className={styles.nav}>
             <div className={styles.image}>
-                <Image src="/logo.svg" width="80" height="80" alt="logo" />
+                <Link href={`/`}>
+                    <Image src="/logo.svg" width="80" height="80" alt="logo" />
+                </Link>
             </div>
             <div className={styles.links}>
                 <Link href={`/about`}>About</Link>
                 <Link href={`/contact`}>Contact</Link>
+                <Link href={`/projects`}>Projects</Link>
                 <Link href={`/links`}>Links</Link>
                 <Link href={`/blog`}>Blog</Link>
             </div>
