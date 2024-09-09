d513681adf88880ab036d7e2cfda1382bcffe19b
Sky Kosiner <sky@skykosiner.com>
2024-09-09 14:19:18 +0100

    commit the dir baby

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
-import createMDX from '@next/mdx'
+import createMDX from "@next/mdx"
 
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
     // Configure `pageExtensions` to include markdown and MDX files
-    pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+    pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
     // Optionally, add any other Next.js config below
+    output: "standalone",
 }
 
 const withMDX = createMDX({
